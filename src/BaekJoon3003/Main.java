6a87e12aff64d085306ede6db949671505988211
jomseni <jomseni642@gmail.com>
2022-08-11 14:33:47 +0900

    백준 코딩테스트 연습 시작 - 3003번

---
+++ src/BaekJoon3003/Main.java
@@ -0,0 +1,30 @@
+package BaekJoon3003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] perfect = {1,1,2,2,2,8};
+
+        int[] mine = new int[6];
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        for(int i = 0; i < 6; i++){
+            mine[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < mine.length ; i++){
+            mine[i] = perfect[i] - mine[i];
+            System.out.print(mine[i]+" ");
+        }
+
+    }
+}
