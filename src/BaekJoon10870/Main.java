18c939d0412e82792cc6dfa5665cf23d4dbe0cf6
jomseni <jomseni642@gmail.com>
2022-08-12 16:54:09 +0900

    백준 코딩테스트 연습 - 10870번 (피보나치 기본)
    피보나치 수열  점화식 만들기!
    
    f(0) --> 0 반환, f(1) --> 1을 반환 한다!
    
    피보나치 수열 점화식 -- > F(N) = F(N-1) + F(N-2) 를 이용하여 0과 1까지 재귀함수를 이용해 분해 후 분해한것을 리턴값을 합치면서 결국 input 값 까지 구해준다

---
+++ src/BaekJoon10870/Main.java
@@ -0,0 +1,29 @@
+package BaekJoon10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        //n번째를 구하기위해 n에 대한 숫자를 입력받는다
+        int input = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonacci(input));
+
+    }
+
+    //파라미터 int 형 input이 num으로 던져진다!
+    static int fibonacci(int num) {
+        if(num == 1) return 1;
+        if(num == 0) return 0;
+
+        return fibonacci(num -1) + fibonacci(num- 2);
+    }
+}
+
