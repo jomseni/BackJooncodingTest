eccb47eb8439f41d40945b49f6b6dc7c97306304
jomseni <jomseni642@gmail.com>
2022-08-12 17:41:26 +0900

    백준 코딩테스트 연습 - 10872번 (팩토리얼 메서드 수행 기본)
    1! = 1 , 0! = 1 을 이용하여 함수를 정의한다
    
    팩토리얼 => n! => n * n-1 * n-2 * ... * 1

--- src/BaekJoon10872/Main.java
+++ src/BaekJoon10872/Main.java
@@ -1,7 +1,26 @@
 package BaekJoon10872;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(input));
+    }
+
+    static int factorial(int num){
+        if( num == 1) return 1;
+        if( num == 0) return 1;
+
+        //ex) input = 10 이라면 10 * (10-1) * ... * 1 이므로
+        //input값 * num-1 파라미터를 가진 factorial 함수가 호출 되어야하므로
+        //return 값을 아래와 같이 호출한다!
+        return num * factorial(num-1);
     }
 }
