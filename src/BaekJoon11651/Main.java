e959a923f8aa1d51cd8369a0cfaf37bc53713267
jomseni <jomseni642@gmail.com>
2022-08-30 21:34:09 +0900

    백준 코딩 테스트  - 11651번 (람다식을 이용한 수 좌표를 정렬)
    Arrays.sort(array, (e1,e2) -> {
    
    //           e 안의 인덱스는 열 기준이다! 1차원 배열이므로! 매우 중요 헷갈린부분!!!
                if(e1[1] == e2[1]){
                    return e1[0] - e2[0];//이 값이 양수 이면 위치 바꾸기!
                }else{
                    return e1[1] - e2[1];
                }
    
            });
    
            를 이용하여 y값은 오름 차순 정렬 x는 y값이 같을 경우 증가하는 순서로 정렬 해준다!

--- src/BaekJoon11651/Main.java
+++ src/BaekJoon11651/Main.java
@@ -1,8 +1,10 @@
 package BaekJoon11651;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,11 +12,39 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        //받을 점의 갯수의 수를 입력 받는다.
+//        여러개의 출력을 해야 하므로 StringBuilder를 사용한다
+        StringBuilder sb = new StringBuilder();
+
+        //점의 갯수 키보드 값 입력받기
+        int N = Integer.parseInt(br.readLine());
+
+        //x좌표와 y좌표를 받아야 하므로 2차원 배열 생성
+        int[][] array = new int[N][2];
 
 
+        //반복문을 통해 주어진 점의 갯수를 x좌표 y좌표로 받는다!
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
+        }
 
+//        2차원 배열에서 정렬 사용하기위해 람다식 이용한다 2차원배열에선 Arrays.sort()로 배열을 정렬 할 수없어
+//          람다식을 이용한 해결!
+        Arrays.sort(array, (e1,e2) -> {
 
+//           e 안의 인덱스는 열 기준이다! 1차원 배열이므로! 매우 중요 헷갈린부분!!!
+            if(e1[1] == e2[1]){
+                return e1[0] - e2[0];//이 값이 양수 이면 위치 바꾸기!
+            }else{
+                return e1[1] - e2[1];
+            }
+
+        });
+
+        for(int i =0; i < N; i++){
+            sb.append(array[i][0] + " " + array[i][1]).append('\n');
+        }
+        System.out.println(sb);
     }
 }
